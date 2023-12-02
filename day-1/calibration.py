d87a138b996f72f09a07480553dcc158c734daf6
Megan Korling <korlingm@gmail.com>
2023-12-01 21:44:11 -0800

    fix comments

--- day-1/calibration.py
+++ day-1/calibration.py
@@ -1,19 +1,16 @@
 # Takes in one string like pqr3stu8vwx
-# Returns the int 38
+# Returns the integer value of the 
+# first and last digit added together like a string
 def get_int_value(calibration_value):
-    # first digit and second digit, set to none
     first = None
     second = None
-    # loop through each value
-    # if it's an int and first digit is none, set to current value
-    # if it's an int and first digit is not none, set second digit to current value
-        # any other integers later in loop overwrite this
+
     for v in calibration_value:
         if v.isdigit():
             if first is None:
                 first = v
             second = v
-    # add first and second together as ints        
+
     return int(first + second)
 
 map = {
@@ -36,7 +33,8 @@ map = {
     "8": "8",
     "9": "9"
 }
-# part two of problem where abcone2threexyz is 13, and is not 22
+
+# Part two of problem where abcone2threexyz is 13, and is not 22
 def get_int_value_2(calibration_value):
     copy = calibration_value
     first = None
@@ -48,6 +46,7 @@ def get_int_value_2(calibration_value):
                     first = map[key]
                 second = map[key]
         copy = copy[1:]
+
     return int(first + second)
 
 # Takes in a list of calibration_values, like pqr3stu8vwx
@@ -57,28 +56,33 @@ def get_int_value_2(calibration_value):
 def calculate_total(calibration_values):
     total_sum = 0
     for value in calibration_values:
+        # Part 1
         # current_int = get_int_value(value)
+
+        # Part 2
         current_int = get_int_value_2(value)
         total_sum += current_int
 
     return total_sum
 
-# values = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f","treb7uchet"]
-# print(calculate_total(values))
-
-
+# Data for submitting answer
 file = open("/Users/megankorling/Developer/advent-of-code-23/day-1/data.txt", "r")
 values2 = []
 for line in file:
     values2.append(line)
-# print(values2)
 
+
+# Test of Part 1 of problem
+# values = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f","treb7uchet"]
+# print(calculate_total(values))
+
+# Submitted Answer
 # print(calculate_total(values2)) #55130
 
-
-# Part TWO
-
+#-----------------------------------
+# Test of Part 2
 values3 = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
 # print(calculate_total(values3))
 
-print(calculate_total(values2))
+# Submitted Answer
+print(calculate_total(values2)) # 54985
